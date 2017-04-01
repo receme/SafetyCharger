2ae3e41c89b426ea40d91faf145e3ebf55a5eef4
tahmid <tahmid1989@gmail.com>
2017-04-02 00:52:19 +0600

    test jenkins

--- app/src/main/java/com/rcmapps/safetycharger/interfaces/BaseView.java
+++ app/src/main/java/com/rcmapps/safetycharger/interfaces/BaseView.java
@@ -1,7 +1,9 @@
 package com.rcmapps.safetycharger.interfaces;
 
 public interface BaseView {
-    void showError(String title,String message);
+    void showError(String title, String message);
+
     String getResourceString(int stringId);
+
     void showToast(String message);
 }
