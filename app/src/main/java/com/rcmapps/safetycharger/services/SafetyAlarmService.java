1abe81f0d79a553ccb7eff13853184f970c57222
tahmid <tahmid1989@gmail.com>
2017-04-01 18:08:41 +0600

    added service in manifest, register power connection status receiver

--- app/src/main/java/com/rcmapps/safetycharger/services/SafetyAlarmService.java
+++ app/src/main/java/com/rcmapps/safetycharger/services/SafetyAlarmService.java
@@ -3,11 +3,13 @@ package com.rcmapps.safetycharger.services;
 
 import android.app.Service;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
 
 public class SafetyAlarmService extends Service {
 
+    private PowerConnectionReceiver powerConnectionReceiver;
     @Override
     public void onCreate() {
         super.onCreate();
@@ -16,9 +18,18 @@ public class SafetyAlarmService extends Service {
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
 
-
+        powerConnectionReceiver = new PowerConnectionReceiver();
+        registerReceiver(powerConnectionReceiver,new IntentFilter(Intent.ACTION_BATTERY_CHANGED));
 
         return START_STICKY;
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        if(powerConnectionReceiver!=null){
+            unregisterReceiver(powerConnectionReceiver);
+        }
     }
 
     @Nullable
