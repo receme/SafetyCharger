bc09a97fa9e0995d6ecfa6056b50574901b9f3d6
tahmid <tahmid1989@gmail.com>
2017-04-01 17:05:57 +0600

    changed jacoco.gradle to fix test coverage report generation problem

---
+++ app/src/main/java/com/rcmapps/safetycharger/services/SafetyAlarmService.java
@@ -0,0 +1,29 @@
+package com.rcmapps.safetycharger.services;
+
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
+
+public class SafetyAlarmService extends Service {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+
+
+
+        return START_STICKY;
+    }
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+}
