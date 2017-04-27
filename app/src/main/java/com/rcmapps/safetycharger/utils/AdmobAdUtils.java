bf70f3a9ba48d435eb268199912d3c24b3b80300
tahmid <tahmid1989@gmail.com>
2017-04-27 22:11:24 +0600

    added app icon, added intertitial ad, screen orientation fixed to portrait

--- app/src/main/java/com/rcmapps/safetycharger/utils/AdmobAdUtils.java
+++ app/src/main/java/com/rcmapps/safetycharger/utils/AdmobAdUtils.java
@@ -7,17 +7,22 @@ import com.google.android.gms.ads.AdListener;
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.InterstitialAd;
 
+import java.util.Random;
+
 public class AdmobAdUtils {
 
     private static final String ADMOB_APP_ID = "ca-app-pub-3996731798982494/4759163715";
 
     Context context;
     InterstitialAd mInterstitialAd;
+    Random random;
 
     public AdmobAdUtils(Context context) {
         this.context = context;
         this.mInterstitialAd = new InterstitialAd(context);
         this.mInterstitialAd.setAdUnitId(ADMOB_APP_ID);
+
+        this.random = new Random();
 
         this.mInterstitialAd.setAdListener(new AdListener() {
             @Override
@@ -36,20 +41,20 @@ public class AdmobAdUtils {
         });
     }
 
-    public void startLoadingAd(){
+    public void startLoadingAd() {
         requestNewInterstitial();
     }
 
-    public void showAd(){
-        if(mInterstitialAd!=null && mInterstitialAd.isLoaded()){
+    public void showAd() {
+        if (mInterstitialAd != null && mInterstitialAd.isLoaded() && random.nextInt() % 3 == 0) {
             mInterstitialAd.show();
         }
     }
 
     private void requestNewInterstitial() {
-        if(UtilMethods.isInternetAvailable(context) && !mInterstitialAd.isLoaded()){
+        if (UtilMethods.isInternetAvailable(context) && !mInterstitialAd.isLoaded()) {
             AdRequest adRequest = new AdRequest.Builder()
-                    .addTestDevice("F0BC99DD3A2447A38D525DBA2A80CFA6")
+                    //.addTestDevice("F0BC99DD3A2447A38D525DBA2A80CFA6")
                     .build();
 
             mInterstitialAd.loadAd(adRequest);
